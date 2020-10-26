93204bbb778fffbd5251403f850222e057e39ff7
lital barak <litalbarak2@gmail.com>
2020-10-26 23:09:20 +0200

    2 and 6 are wrong

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -8,7 +8,7 @@
 #include "anomaly_detection_util.h"
 // פונקציה של חישוב ממוצע
 float avg(float* x, int size){
-    float sum = 0;
+    float sum =0;
     for (int i=0;i<size;i++){
         sum=sum+x[i];
     }
@@ -19,14 +19,14 @@ float avg(float* x, int size){
 
 // returns the variance of X and Y
 float var(float* x, int size){
-    float sigma;//? לצרף או לשים אפס,אם גודל האוכלוסיה הוא 1 האם המיו הוא 0 או 1
-    for(int i =0;i<=size;i++){//לולאה למיו
-        sigma =sigma+x[i];// מקווה שזה בסדר,לבדוק את זה אחרי זה ,מבחינת פוינטרים אם זה עושה בעיה
+    float sigma=0;
+    for(int i =0;i<size;i++){//לולאה לסיגמא למיו
+        sigma =sigma+x[i];
     }
     //אני יכולה
     float mew = (sigma/size);//בין 2 INT התוצואה תיתן 0 כי זה קטן מ 1 לכן היה אפשר להמיר ל FLOAT או לוותר לגמרי על ה ה1
     // אני מבינה שיש דרך ליעול מכתיבת שתי לולאות אבל זה ממש מוזר
-    float myVarSum;
+    float myVarSum=0;
     for(int i =0;i<size;i++){//סיגמא לVAR
         myVarSum=myVarSum+((x[i]-mew)*(x[i]-mew));
 
@@ -39,21 +39,21 @@ float var(float* x, int size){
 
 // returns the covariance of X and Y
 float cov(float* x, float* y, int size){
-    float sigmaX;
+    float sigmaX=0;
     for(int i =0;i<size;i++){//לולאה למיו
         sigmaX =sigmaX+x[i];
     }
     float MewX =sigmaX/size;
 
-    float sigmaY;
+    float sigmaY=0;
     for(int i =0;i<size;i++){//לולאה למיו
         sigmaY =sigmaY+y[i];
     }
     float MewY =sigmaY/size;
 
-    float sigmaXY=sigmaX+sigmaY;
+    float sigmaXY=0;
     for(int i =0;i<size;i++){//לולאה למיו
-        sigmaXY =sigmaXY+x[i]+y[i];
+        sigmaXY =sigmaXY+(x[i]*y[i]);
     }
     float MewXY =sigmaXY/size;
     float COV = MewXY - MewX*MewY;
