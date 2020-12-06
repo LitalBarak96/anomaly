762e7ef4aa065f1a9b6039508e12d420da18f473
lital barak <litalbarak2@gmail.com>
2020-12-06 12:29:33 +0200

    normal done ?

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -12,13 +12,23 @@ SimpleAnomalyDetector::~SimpleAnomalyDetector() {
 
 
 void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
+    float mypers=0;
+    int myMostcorFeatur=0;
     vector<vector<float>>mytablevector=ts.getthetable();
     int sizeofcolom=mytablevector[1].size();//כמה עמודות יש
-    for (int j = 0;j<sizeofcolom;j++){
+    for (int j = 0;j<sizeofcolom;j++){//לולאה על זה שאנחנו בודקים
         float theFirst [mytablevector.size()];// המערך שאליו בודקים הראשון
         float theOthers [mytablevector.size()];// המערך שבודקים אליו
         std::copy(mytablevector.at(j).begin(), mytablevector.at(j).end(), theFirst);//העתקה של הוקטור לתוך המערך
-        for (int i=1;i< )
+        for (int i=j+1;i<sizeofcolom;i++ ){// בודקת בלולאה הזו על כל השאר ולא צריך לבדוק אחורה,מקווה שזה לא יעשה שגיאה של יציאה מגבולות
+            std::copy(mytablevector.at(i).begin(), mytablevector.at(i).end(), theOthers);
+            if (mypers<abs(pearson(theFirst,theOthers,sizeofcolom))){
+                mypers=abs(pearson(theFirst,theOthers,sizeofcolom));// בערך מוחלט
+                myMostcorFeatur=i;  //   אני מחזיקה את הערך
+            }
+
+
+        }
 
 
 }
